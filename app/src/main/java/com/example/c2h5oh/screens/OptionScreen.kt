41f365dcad0ded650014ee1bf6e62fb388f0db69
jyjyjy <164116320+joyoon1213@users.noreply.github.com>
2025-06-06 14:29:37 +0900

    수정

--- app/src/main/java/com/example/c2h5oh/screens/OptionScreen.kt
+++ app/src/main/java/com/example/c2h5oh/screens/OptionScreen.kt
@@ -9,8 +9,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
-import androidx.compose.material.Text
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -19,6 +19,7 @@ import androidx.compose.ui.unit.sp
 
 @Composable
 fun OptionScreen(
+    onBackClick: () -> Unit,
     onTagOptionClick: () -> Unit,
     onSearchOptionClick: () -> Unit
 ) {
