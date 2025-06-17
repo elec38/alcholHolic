be7653774273a8465f40489b3ee6e9026540b3c2
red <jchoe1389@gmail.com>
2025-06-17 19:02:37 +0900

    db 연동 및 태그 검색 구현

--- app/src/main/java/com/example/c2h5oh/DetailActivity.kt
+++ app/src/main/java/com/example/c2h5oh/DetailActivity.kt
@@ -3,6 +3,11 @@ package com.example.c2h5oh
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.navigation.compose.rememberNavController
 import com.example.c2h5oh.screens.DetailScreen
 import com.example.c2h5oh.theme.C2h5ohTheme
@@ -16,8 +21,15 @@ class DetailActivity : ComponentActivity() {
         setContent {
             val navController = rememberNavController()
             C2h5ohTheme {
+                var liquorList by remember { mutableStateOf<List<Liquor>>(emptyList()) }
+
+                LaunchedEffect(true) {
+                    val result = fetchLiquorsByTags(selectedTags)
+                    liquorList = result
+                }
                 DetailScreen(
                     tags = selectedTags,
+                    liquors = liquorList,
                     onBackClick = { navController.popBackStack() }
                 )
             }
